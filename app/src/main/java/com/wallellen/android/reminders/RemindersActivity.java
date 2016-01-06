0c054c674155ac7ff94c35a752faf39bf1c87abc
wallellen <wallellen@hotmail.com>
2016-01-06 18:28:54 +0800

    adds contextual action mode with context action menu

--- app/src/main/java/com/wallellen/android/reminders/RemindersActivity.java
+++ app/src/main/java/com/wallellen/android/reminders/RemindersActivity.java
@@ -1,5 +1,7 @@
 package com.wallellen.android.reminders;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,10 +9,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -56,12 +62,77 @@ public class RemindersActivity extends AppCompatActivity {
 
         mListView.setAdapter(mCursorAdapter);
 
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        mListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(RemindersActivity.this, "Clicked " + position, Toast.LENGTH_SHORT).show();
+            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                MenuInflater inflater = mode.getMenuInflater();
+                inflater.inflate(R.menu.cam_menu, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_item_delete_reminder:
+                        for (int c = mCursorAdapter.getCount() - 1; c >= 0; c--) {
+                            if (mListView.isItemChecked(c)) {
+                                mDbAdapter.deleteReminderById((int) mCursorAdapter.getItemId(c));
+                            }
+                        }
+
+                        mode.finish();
+                        mCursorAdapter.changeCursor(mDbAdapter.fetchAllReminders());
+                        return true;
+                }
+
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+
             }
         });
+
+
+//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, final int masterListPosition, long id) {
+////                Toast.makeText(RemindersActivity.this, "Clicked " + position, Toast.LENGTH_SHORT).show();
+//                AlertDialog.Builder builder = new AlertDialog.Builder(RemindersActivity.this);
+//                ListView modeListView = new ListView(RemindersActivity.this);
+//                String[] modes = new String[]{"Edit Reminder", "Delete Reminder"};
+//                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(RemindersActivity.this, R.layout.reminders_row, R.id.row_text, modes);
+//                modeListView.setAdapter(arrayAdapter);
+//                builder.setView(modeListView);
+//                final Dialog dialog = builder.create();
+//                dialog.show();
+//
+//                modeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                    @Override
+//                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                        if (position == 0) {
+//                            Toast.makeText(parent.getContext(), "edit " + masterListPosition, Toast.LENGTH_SHORT).show();
+//                        } else {
+//                            Toast.makeText(parent.getContext(), "delete " + masterListPosition, Toast.LENGTH_SHORT).show();
+//                        }
+//
+//                        dialog.dismiss();
+//                    }
+//                });
+//            }
+//        });
     }
 
     private void initData(Bundle savedInstanceState) {
