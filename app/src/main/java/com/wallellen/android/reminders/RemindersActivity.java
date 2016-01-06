6d41f836e5728044d40d801ecfb70f09b4ee68c5
wallellen <wallellen@hotmail.com>
2016-01-06 13:39:52 +0800

    before commit

--- app/src/main/java/com/wallellen/android/reminders/RemindersActivity.java
+++ app/src/main/java/com/wallellen/android/reminders/RemindersActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -53,10 +54,21 @@ public class RemindersActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+//        if (id == R.id.action_new || id == R.id.action_exit) {
+//            return true;
+//        }
+
+        switch (item.getItemId()) {
+            case R.id.action_new:
+                Log.d(getLocalClassName(), "Create new Reminder");
+                return true;
+            case R.id.action_exit:
+                finish();
+                Log.d(getLocalClassName(), "Exit");
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
 }
